03d8102237f1c41424ea50de9164c4874b0c1128
Dmitriy Korn <d.kornaukhov@tsekh.design>
2023-11-02 16:25:09 +0400

    Add some fix

--- src/MyButton/MyButton.js
+++ src/MyButton/MyButton.js
@@ -3,18 +3,18 @@ import React from 'react';
 import './MyButton.scss'
 
 /**
- * @param {Object} props - Пропсы компонента MyButton.
- * @param {string} [props.className] - Класс для кнопки.
- * @param {React.ReactNode} [props.children='Кнопка'] - Содержимое кнопки.
- * @param {string} [props.variant='primary'] - Вариант кнопки ('primary' или 'secondary').
- * @param {("small"|"medium"|"large")} [props.size="medium"] - Размер кнопки.
- * @param {boolean} [props.disabled=false] - Флаг, указывающий на то, отключена ли кнопка.
- * @param {Object} [props.styles] - Инлайн стили для кнопки.
- * @param {Function} [props.onClick] - Обработчик клика на кнопку.
- * @param {Function} [props.onChange] - Обработчик изменения кнопки.
+ // * @param {Object} props - Пропсы компонента MyButton.
+ * @param {string} [className] - Класс для кнопки.
+ * @param {(JSX.Element|undefined)} children - Содержимое кнопки.
+ * @param {string} [variant='primary'] - Вариант кнопки ('primary' или 'secondary').
+ * @param {("small"|"medium"|"large")} [size="medium"] - Размер кнопки.
+ * @param {boolean} [disabled=false] - Флаг, указывающий на то, отключена ли кнопка.
+ * @param {(CSSProperties|undefined)} [styles] - Инлайн стили для кнопки.
+ * @param {Function} [onClick] - Обработчик клика на кнопку.
+ * @param {Function} [onChange] - Обработчик изменения кнопки.
  */
 
-export const MyButton = ({className, children = 'Кнопка', variant= 'primary', size = 'medium', disabled = false, styles, onClick, onChange}) => {
+export const MyButton = ({className, children = 'Кнопкааа', variant= 'primary', size = 'medium', disabled = false, styles, onClick, onChange}) => {
   const mode = variant === 'primary' ? 'Btn__primary' : 'Btn__secondary';
   const sizeClass = `Btn__${size}`;
   const disabledClass = disabled ? 'BtnDisabled' : ''
